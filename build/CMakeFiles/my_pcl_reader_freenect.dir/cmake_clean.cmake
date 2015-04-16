9a2d28e6e56037323bf96b09fadab12f0502998c
GiorgioMire <giorgio.mirenda@gmail.com>
2015-04-16 18:32:38 +0200

    Correzioni suggerite da Giacomo (ancora problema di lentezza)

---
+++ build/CMakeFiles/my_pcl_reader_freenect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/my_pcl_reader_freenect.dir/my_pcl_reader_freenect.cpp.o"
+  "my_pcl_reader_freenect.pdb"
+  "my_pcl_reader_freenect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/my_pcl_reader_freenect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
